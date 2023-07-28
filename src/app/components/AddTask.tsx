a454b697aa07aa575feffacf04d9f0d64a91abad
noaysk <nyasukagawa@myseneca.ca>
2023-07-28 17:16:31 -0400

    created add new task function

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -1,12 +1,36 @@
+"use client"; // This is a client component
+import { FormEventHandler, useState } from "react";
+import { addTodo } from "../../../api";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from 'uuid';
+
 const AddTask = () => {
+  const router = useRouter();
+  const [newTaskValue, setNewTaskValue] = useState<string>("");
+
+  const handleSubmitNewTodo: FormEventHandler<HTMLElement> = async (e) => {
+    e.preventDefault();
+    await addTodo({
+      id: uuidv4(), // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+      text: newTaskValue
+    })
+    setNewTaskValue("");
+    router.refresh();
+  };
   return (
     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center my-0 mx-auto">
-      <input
-        type="text"
-        placeholder="Enter Title"
-        className="input input-bordered w-full max-w-xs"
-      />
-      <button className="btn btn-primary">ADD</button>
+      <form onSubmit={handleSubmitNewTodo}>
+        <input
+          value={newTaskValue}
+          onChange={(e) => setNewTaskValue(e.target.value)}
+          type="text"
+          placeholder="Enter Title"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <button type="submit" className="btn btn-primary">
+          ADD
+        </button>
+      </form>
     </div>
   );
 };
